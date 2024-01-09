65ecc3d9c6e084ffb3da28eed32fe117149ccc96
Massmon <centomass@gmail.com>
2024-01-08 19:46:22 -0500

    uijghg

---
+++ Project1/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Drawing;
+
+public class Class1
+{
+	public Class1(int x, int y, int width, int height)
+	{
+		 Rectangle Button = new Rectangle(x, y, width, height);
+
+	}
+}
